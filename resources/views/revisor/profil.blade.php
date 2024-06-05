9e2c6a5bbf2dc6861ab1e8e3de723f26ef97bf81
Antonio <antoniominafra.dev@gmail.com>
2024-06-05 22:56:46 +0200

    iniziata view del profilo

--- resources/views/revisor/profil.blade.php
+++ resources/views/revisor/profil.blade.php
@@ -1,3 +1,21 @@
 <x-layout>
 
+    <div class="container-fluid vh-100 mt-5">
+        <div class="row align-items-center justify-content-center ">
+            <div class="col-6">
+                <h3 class=" display-3 text-center position-relative">nome e cognome</h3>
+                <img class=" position-relative rounded-circle img-fluid" src="{{Storage::url('public/img/profil-defaul.jpeg')}}" alt="">
+            </div>
+            <div class="col-6 d-flex">
+                <div class="ms-5">
+                    <h4>Dati Personali</h4>
+                    <p>nome:</p>
+                    <p>email:</p>
+                    <p>sei un revisore?</p>
+                    <p>quanti articoli hai creato?</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </x-layout>
